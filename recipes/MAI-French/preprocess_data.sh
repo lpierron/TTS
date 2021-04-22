1a98f9dc24ece331c8962e0834d9f2cdf6427e9b
Laurent Pierron <Laurent.Pierron@inria.fr>
2021-04-22 17:53:19 +0200

    adding conda setup in train_model.sh

--- recipes/MAI-French/preprocess_data.sh
+++ recipes/MAI-French/preprocess_data.sh
@@ -1,6 +1,19 @@
  #!/bin/bash -l
 
-if [ -f ~/.bashrc ]; then . ~/.bashrc; fi
+ # >>> conda initialize >>>
+ # !! Contents within this block are managed by 'conda init' !!
+ __conda_setup="$('/home/lpierron/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+ if [ $? -eq 0 ]; then
+     eval "$__conda_setup"
+ else
+     if [ -f "/home/lpierron/miniconda3/etc/profile.d/conda.sh" ]; then
+         . "/home/lpierron/miniconda3/etc/profile.d/conda.sh"
+     else
+         export PATH="/home/lpierron/miniconda3/bin:$PATH"
+     fi
+ fi
+ unset __conda_setup
+ # <<< conda initialize <<<
 
  # PATHS
  home_path="/srv/storage/talc3@talc-data.nancy/multispeech/calcul/users/lpierron"
